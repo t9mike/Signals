4ea283417e670c1c568cd13d6d81db076c0926c6
Michael S Muegel <mike@muegel.org>
2026-06-10 10:36:49 -0500

    Fix for Xcode 27

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Signals",
+    platforms: [
+        .watchOS("9.0")
+    ],
     products: [
         .library(name: "Signals", targets: ["Signals"]),
     ],
